17b43b4dbba90fbb8f5b86d1be4390a8a9999f8d
Javier Ledezma <jledezma@medtrainer.com>
2020-02-15 23:46:55 -0600

    Replace double quotes on InvalidLastHoursException

--- src/Exception/DataParameters/InvalidLastHoursException.php
+++ src/Exception/DataParameters/InvalidLastHoursException.php
@@ -15,7 +15,7 @@ class InvalidLastHoursException extends InvalidArgumentException
      */
     public function __construct($code = 0, Throwable $previous = null)
     {
-        $message = "Last hours could not be lower than 0.";
+        $message = 'Last hours could not be lower than 0.';
         parent::__construct($message, $code, $previous);
     }
 }
