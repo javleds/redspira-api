b2f906031a65ea4477741c67f87ef2ededb322ed
Javier Ledezma <jledezma@medtrainer.com>
2020-03-02 21:42:47 -0600

    Add facades to composer.json and BaseTestCase

--- tests/BaseTestCaste.php
+++ tests/BaseTestCaste.php
@@ -2,7 +2,10 @@
 
 namespace Javleds\RedspiraApi\Tests;
 
+use Javleds\RedspiraApi\Facade\AreaRegistryRepository;
+use Javleds\RedspiraApi\Facade\AreaRepository;
 use Javleds\RedspiraApi\Facade\DeviceRegistryRepository;
+use Javleds\RedspiraApi\Facade\DeviceRepository;
 use Javleds\RedspiraApi\Facade\RedspiraApi;
 use Javleds\RedspiraApi\RedspiraApiServiceProvider;
 use Orchestra\Testbench\TestCase;
@@ -21,6 +24,9 @@ class BaseTestCaste extends TestCase
         return [
             'RedspiraApi' => RedspiraApi::class,
             'DeviceRegistryRepository' => DeviceRegistryRepository::class,
+            'DeviceRepository' => DeviceRepository::class,
+            'AreaRepository' => AreaRepository::class,
+            'AreaRegistryRepository' => AreaRegistryRepository::class,
         ];
     }
 }
